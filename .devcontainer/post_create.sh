b6c45cc8665e6f6dcfd51c458b6f1e4031ee9caf
Mattias Thalén <bitter-polders0x@icloud.com>
2026-03-15 20:54:10 +0000

    feat: add gh setup to post create

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-# macOS/Linux: Extract and move to PATH
+
+# Log into GitHub and configure git
+gh auth login -p ssh
+git config --global user.name "$(gh api user -q .name)"
+git config --global user.email "$(gh api user -q .email)"
+git config --global gpg.format ssh
+git config --global user.signingkey ~/.ssh/id_ed25519.pub
+git config --global commit.gpgsign true
+git config --global tag.gpgsign true
+echo "$(git config --global user.email) $(cat ~/.ssh/id_ed25519.pub)" > ~/.ssh/allowed_signers
+git config --global gpg.ssh.allowedSignersFile ~/.ssh/allowed_signers
+
+# Install daana-cli
 tar -xzf daana-cli_0.5.17_linux_amd64.tar.gz
 sudo mv daana-cli /usr/local/bin/
- 
-# Verify installation
 daana-cli --version
